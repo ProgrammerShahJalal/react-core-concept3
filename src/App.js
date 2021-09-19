b9002f9ae86c420936a40e69fd9933bcde81be59
Md Shah Jalal <bestwebdesignerr@gmail.com>
2021-09-19 17:39:29 +0600

    summury

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
+      <LoadPost></LoadPost>
     </div>
   );
 }
+function LoadPost() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(res => res.json())
+      .then(data => setPosts(data))
+  }, [])
 
+
+  return (
+    <div>
+      <h2>Load Posts</h2>
+      {
+        posts.map(post => <Post title={post.title} body={post.body}></Post>)
+      }
+    </div>
+  )
+}
+function Post(props) {
+  return (
+    <div style={{ color: 'white', margin: "35px", backgroundColor: 'darkblue', borderRadius: '25px', padding: '10px' }}>
+      <h3>{props.title}</h3>
+      <p>{props.body}</p>
+    </div>
+  );
+}
+function Counter() {
+  const [count, setCount] = useState(0);
+  const handlerIncrease = () => setCount(count + 1);
+  const handlerDecrease = () => setCount(count - 1);
+  const clear = () => setCount(count - count);
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={handlerIncrease}>Increase</button>
+      <button onClick={handlerDecrease}>Decrease</button>
+      <button onClick={clear}>Clear</button>
+    </div>
+  )
+}
 export default App;
